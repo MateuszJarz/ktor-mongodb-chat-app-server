49a80bf7fb038656ef07c40214f8d03cc0d6bc31
Mat <62247805+MateuszKapelusz@users.noreply.github.com>
2022-05-03 19:05:56 +0200

    Create model Message /MessageDataSource + MessageDataSourceImpl

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,7 +36,12 @@ dependencies {
 
     //MONGO
     implementation("org.litote.kmongo:kmongo:$kmongo_version")
+    implementation("org.litote.kmongo:kmongo-coroutine:$kmongo_version")
 
     //KOIN
     implementation( "io.insert-koin:koin-core:$koin_version")
+    // Koin for Ktor
+    implementation ("io.insert-koin:koin-ktor:$koin_version")
+// SLF4J Logger
+    implementation ("io.insert-koin:koin-logger-slf4j:$koin_version")
 }
---
+++ src/main/kotlin/com/example/data/model/Message.kt
@@ -0,0 +1,18 @@
+package com.example.data.model
+
+import kotlinx.serialization.Serializable
+import org.bson.codecs.pojo.annotations.BsonId
+import org.bson.types.ObjectId
+import javax.print.attribute.standard.JobOriginatingUserName
+
+@Serializable
+data class Message(
+
+    @BsonId
+    val id: String = ObjectId().toString(),
+
+    val text: String,
+    val userName: String,
+    val timesTamp: Long
+
+)
